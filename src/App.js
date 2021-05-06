7630e9a1089dc8c8be0b56979c12996bef563392
DAltier <danaaltier@hotmail.com>
2021-05-06 15:51:27 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import './App.css';
+import HookForm from './components/HookForm';
+import User from './components/User';
+
+function App() {
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  return (
+    <div className="App">
+      <HookForm imput = { state } setInput = { setState }/>
+      <User data = { state }/>
+    </div>
+  );
+}
+
+export default App;
